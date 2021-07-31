fd9df71047721364813a99e5c7725833ac69489f
Arjun Senthil <arjunsenthilks2001@gmail.com>
2021-07-31 20:38:42 +0530

    done

--- substrings.rb
+++ substrings.rb
@@ -9,8 +9,12 @@ def substrings(text, words)
     text = text.join(" ")
 
     words.each do |word|
-        puts text.scan(/(?=#{word})/).count
+        word_count = text.scan(/(?=#{word})/).count
+        if word_count > 0
+            result[word] = word_count
+        end
     end 
+    puts result
 end
 
 substrings("Howdy partner, sit down! How's it going?",dictionary)
