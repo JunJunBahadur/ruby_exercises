b185f713417c4f3e90ee92c8efb402517344fbae
Arjun Senthil <arjunsenthilks2001@gmail.com>
2021-08-01 11:27:23 +0530

    3rd exercise

--- substrings.rb
+++ substrings.rb
@@ -3,7 +3,7 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 def substrings(text, words)
     result = Hash.new(0)
     words.each do |word|
-        word_count = text.downcase.scan(/(?=#{word})/).count
+        word_count = text.downcase.scan(word).count
         result[word] = word_count if word_count > 0
     end 
     puts result
